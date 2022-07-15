b7e24f424d640789ee82e3878865a3902b478fba
Sadeeed <sadeeedw@gmail.com>
2022-07-15 10:35:01 +0500

    :package: add edit recipe functionality

--- apps/main/views.py
+++ apps/main/views.py
@@ -23,6 +23,7 @@ def recipe(slug):
         name = recipe.name
         return render_template('recipe.html', name=name, ingredients=ingredients, method=method, id=recipe.id)
     else:
+        flash("This recipe does not exist")
         return redirect(url_for('main.index'))
 
 
@@ -53,12 +54,40 @@ def add_recipe():
 @login_required
 def del_recipe(recipe_id):
     recipe = Recipe.query.filter_by(id=recipe_id).first()
-    if recipe is not None:
+    if recipe:
         db.session.delete(recipe)
         db.session.commit()
     else:
-        flash("The recipe does not exist")
+        flash("This recipe does not exist")
     return redirect(url_for('main.index'))
+
+
+@main.route("/recipe/edit/<recipe_id>", methods=['GET', 'POST'])
+@login_required
+def edit_recipe(recipe_id):
+    recipe = Recipe.query.filter_by(id=recipe_id).first()
+    if recipe:
+        if request.method == 'GET':
+            name = recipe.name
+            ingredients = recipe.ingredients
+            method = recipe.method
+            return render_template('edit_recipe.html', name=name, ingredients=ingredients, method=method)
+        if request.method == 'POST':
+            recipe.name = request.form['name']
+            recipe.method = request.form['method']
+
+            mutable_request = request.form.copy()
+            mutable_request.pop('name')
+            mutable_request.pop('method')
+
+            for ingredient, new_ingredient in zip(recipe.ingredients, mutable_request.values()):
+                ingredient.name = new_ingredient
+            db.session.commit()
+
+            return redirect(url_for('main.recipe', slug=recipe.slug))
+    else:
+        flash("This recipe does not exist")
+        return redirect(url_for('main.index'))
 
 
 @main.route("/profile")
